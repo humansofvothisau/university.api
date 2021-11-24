5b9435db70a74c6e5227334e9b4d6b0744298fb7
Tran Phong <n.tranphongse@gmail.com>
2021-11-24 22:21:41 +0700

    Add API project with initial fiels without any code yet

---
+++ university.api/Controllers/THPT/TotNghiepThptController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace university.api.Controllers.THPT
+{
+    public class TotNghiepThptController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
