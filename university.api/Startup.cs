5bc71249b80ef59570fe87b41ffa1776df15a4db
Tran Phong <57389575+ntrphongse@users.noreply.github.com>
2021-12-06 21:12:50 +0700

    Update Startup.cs

--- university.api/Startup.cs
+++ university.api/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -57,6 +58,10 @@ namespace university.api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "university.api v1"));
             }
 
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    });
             app.UseHttpsRedirection();
 
             app.UseRouting();
